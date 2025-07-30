7921369f49365032b3590d2b71781266aa89cc89
Your Name <rajansingh70707@gmail.com>
2025-07-30 13:19:12 +0530

    setUp smpt using nodemailer

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyparser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
 
+const { sendBasicEmail } = require('./services/email-service');
 
 const app = express();
 
@@ -10,8 +11,16 @@ const serverSetup = () => {
         app.use(bodyparser.urlencoded({ extended: true}));
 
         app.listen(PORT, () => {
-            console.log(`server is running at ${PORT}`)
+            console.log(`server is running at ${PORT}`);    
+
+            // sendBasicEmail(
+            //     'support@admin.com',
+            //     'kumarrajansingh49@gmail.com',
+            //     'This is a testing email',
+            //     'Hey, how are you, I hope you like the support'
+            // );
         });
 }
 
 serverSetup();
+
---
+++ src/services/email-service.js
@@ -0,0 +1,20 @@
+const sender = require ('../config/emailConfig');
+
+const sendBasicEmail = async (mailFrom, mailTo, mailSubject, mailBody) => {
+    try {
+        const reponse = await sender.sendMail({
+        from: mailFrom,
+        to: mailTo,
+        subject: mailSubject,
+        text: mailBody
+    });
+    console.log(reponse);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports = {
+    sendBasicEmail
+}
+
